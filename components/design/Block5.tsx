d5470c1b8049449d6217f83de15fd88a916c28c2
upvisor <admin@upvisor.cl>
2025-10-06 19:00:01 -0300

    actualization general

--- components/design/Block5.tsx
+++ components/design/Block5.tsx
@@ -198,7 +198,7 @@ export const Block5 = ({ content, index, calls, forms, design, payment, style, s
           </div>
           {
             content.info?.image && content.info.image !== ''
-              ? <Image ref={imageRef} className={`${imageLoaded ? 'opacity-1' : 'opacity-0 translate-y-6'} transition-all duration-500 h-fit mx-auto mt-4`} style={{ boxShadow: style.design === 'Sombreado' ? `0px 3px 20px 3px ${style.borderColor}10` : '', borderRadius: style.form === 'Redondeadas' ? `${style.borderBlock}px` : '', border: style.design === 'Borde' ? `1px solid ${style.borderColor}` : '' }} width={700} height={500} alt='Imagen slider prueba' src={content.info.image} />
+              ? <Image ref={imageRef} className={`${imageLoaded ? 'opacity-1' : 'opacity-0 translate-y-6'} transition-all duration-500 h-fit mx-auto mt-4 max-w-[700px] w-full`} style={{ boxShadow: style.design === 'Sombreado' ? `0px 3px 20px 3px ${style.borderColor}10` : '', borderRadius: style.form === 'Redondeadas' ? `${style.borderBlock}px` : '', border: style.design === 'Borde' ? `1px solid ${style.borderColor}` : '' }} width={1400} height={1000} alt='Imagen slider prueba' src={content.info.image} />
               : ''
           }
         </div>
